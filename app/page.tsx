26905cb64464c2311c7ae947f4f24c71a03b7cb7
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-29 08:02:15 +0530

    background colour changed

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const HomePage = () => {
-  return <div className="text-5xl text-red-950">HomePage</div>;
+  return <div>HomePage</div>;
 };
 
 export default HomePage;
