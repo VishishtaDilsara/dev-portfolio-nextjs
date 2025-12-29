4958bbf61f840a560baa36b85dd4886b285d178c
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-29 15:17:37 +0530

    react multi carousel package added

---
+++ components/Home/ClientReview/ClientReview.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ClientReview = () => {
+  return (
+    <div className="pt-16 pb-16">
+      <h1 className="text-center text-2xl md:text-4xl xl:text-5xl font-bold text-white">
+        Kind of words from satisfies <br />
+        <span className="text-cyan-300">clients</span>
+      </h1>
+      <div className="mt-16 w-[70%] mx-auto"></div>
+    </div>
+  );
+};
+
+export default ClientReview;
