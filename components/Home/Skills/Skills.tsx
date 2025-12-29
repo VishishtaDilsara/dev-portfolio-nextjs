7644c3107b00366fee9cc20df331858553ce0f7c
VishishtaDilsara <rgvdilsara2002@gmail.com>
2025-12-29 15:12:07 +0530

    Skills section created

--- components/Home/Skills/Skills.tsx
+++ components/Home/Skills/Skills.tsx
@@ -1,3 +1,4 @@
+"use client";
 import React from "react";
 import {
   SiJavascript,
@@ -7,6 +8,8 @@ import {
   SiTailwindcss,
   SiTypescript,
 } from "react-icons/si";
+
+import Tilt from "react-parallax-tilt";
 
 const skills = [
   {
@@ -43,7 +46,14 @@ const Skills = () => {
       </h1>
       <div className="flex flex-wrap justify-center gap-6 mt-16">
         {skills.map((skill) => {
-          return;
+          return (
+            <Tilt key={skill.name} scale={1.5} transitionSpeed={400}>
+              <div className="bg-[#14134145] text-center w-40 h-48 rounded-3xl flex flex-col items-center justify-center shadow-lg transition hover:scale-80 duration-300">
+                <div className="text-5xl mb-4 text-gray-300">{skill.icon}</div>
+                <p className="text-purple-400 mt-1">{skill.name}</p>
+              </div>
+            </Tilt>
+          );
         })}
       </div>
     </div>
