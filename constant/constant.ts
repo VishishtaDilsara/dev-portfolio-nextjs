9b3ed4aeeb33667991cfca43609dbe517adb2241
VishishtaDilsara <rgvdilsara2002@gmail.com>
2026-02-01 00:53:45 +0530

    Nav bar updated

--- constant/constant.ts
+++ constant/constant.ts
@@ -1,9 +1,8 @@
 export const NavLinks = [
   { id: 1, url: "/#home", label: "Home" },
-  { id: 2, url: "/#services", label: "Services" },
-  { id: 3, url: "/#resume", label: "Resume" },
+  { id: 2, url: "/#resume", label: "Resume" },
+  { id: 3, url: "/#certifications", label: "Certifications" },
   { id: 4, url: "/#projects", label: "Projects" },
-  { id: 5, url: "/#skills", label: "Skills" },
-  { id: 6, url: "/#blogs", label: "Blogs" },
-  { id: 7, url: "/#contact", label: "Contact" },
+  { id: 5, url: "/#blogs", label: "Blogs" },
+  { id: 6, url: "/#contact", label: "Contact" },
 ];
